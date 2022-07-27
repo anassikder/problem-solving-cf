e8e307c4dc023df7f9130e8e4919f8cedae2026b
Anas Sikder <anassikder.aust@gmail.com>
2022-07-27 20:25:07 +0600

    commit

---
+++ skeleton.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solution(){
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << fixed << setprecision(2);
+
+    int test_cases;
+    cin >> test_cases;
+    while(test_cases--){
+        solution();
+    }
+
+    return 0;
+}
